4859fd88a219819d4ac0abd97fa9a066bf88cbd9
karanrajan12 <karanrajanr@gmail.com>
2026-07-05 20:48:21 +0530

    auth features added to the movie routes

--- routes/route.movie.js
+++ routes/route.movie.js
@@ -1,12 +1,29 @@
 import moviecontrollers from '../controllers/controller.movie.js';
 import moviemiddlewares from "../middlewares/middleware.movie.js";
+import authMiddlewares from "../middlewares/middleware.auth.js";
 
 const movieRoutes=(app)=>{
-    app.post('/moviebooking/api/v1/movies',moviemiddlewares.checkCreateMovieReq,moviecontrollers.postController);
+    app.post('/moviebooking/api/v1/movies',
+        authMiddlewares.checkAuthentication,
+        authMiddlewares.isAdminOrClient,
+        moviemiddlewares.checkCreateMovieReq,
+        moviecontrollers.postController
+    );
     app.get('/moviebooking/api/v1/movies/:id',moviecontrollers.getController);
-    app.delete('/moviebooking/api/v1/movies/:id',moviecontrollers.deleteController);
-    app.put('/moviebooking/api/v1/movies/:id',moviecontrollers.updateController);
-    app.patch('/moviebooking/api/v1/movies/:id',moviecontrollers.updateController);
+    app.delete('/moviebooking/api/v1/movies/:id',
+        authMiddlewares.checkAuthentication,
+        authMiddlewares.isAdminOrClient,
+        moviecontrollers.deleteController
+    );
+    app.put('/moviebooking/api/v1/movies/:id',
+        authMiddlewares.checkAuthentication,
+        authMiddlewares.isAdminOrClient,
+        moviecontrollers.updateController);
+    app.patch('/moviebooking/api/v1/movies/:id',
+        authMiddlewares.checkAuthentication,
+        authMiddlewares.isAdminOrClient,
+        moviecontrollers.updateController
+    );
     app.get('/moviebooking/api/v1/movies',moviecontrollers.fetchController);
 }
 
